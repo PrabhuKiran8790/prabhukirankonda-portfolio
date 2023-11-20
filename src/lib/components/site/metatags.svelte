6b6ff5903f912ef01e2c8b4df8399049f676ba54
Prabhu Kiran Konda <prabhukiran8790@pm.me>
2023-11-20 17:30:42 +0530

    added OG

--- src/lib/components/site/metatags.svelte
+++ src/lib/components/site/metatags.svelte
@@ -6,13 +6,14 @@
 {#if $page.url.pathname == '/'}
 	<MetaTags
 		title="Home | Prabhu Kiran Konda"
+		description="Personal website and blog"
 		openGraph={{
 			url: $page.url.href,
 			title: 'Prabhu Kiran Konda | Home',
 			description: 'Personal website and blog',
 			images: [
 				{
-					url: '/og.png',
+					url: 'https://prabhukirankonda.vercel.app/og.png',
 					width: 800,
 					height: 600,
 					alt: 'Prabhu Kiran Konda'
@@ -25,20 +26,20 @@
 			cardType: 'summary_large_image',
 			title: 'Prabhu Kiran Konda | Home',
 			description: 'Personal website and Blog',
-			image: '/og.png',
+			image: 'https://prabhukirankonda.vercel.app/og.png',
 			imageAlt: 'Prabhu Kiran Konda'
 		}}
 	/>
 {/if}
 
 {#if $page.url.pathname == '/blog'}
-	<MetaTags title="Blog | Prabhu Kiran Konda" />
+	<MetaTags title="Blog | Prabhu Kiran Konda" description="Blog" />
 {/if}
 
 {#if $page.url.pathname == '/projects'}
-	<MetaTags title="Projects | Prabhu Kiran Konda" />
+	<MetaTags title="Projects | Prabhu Kiran Konda" description="Projects" />
 {/if}
 
 {#if $page.url.pathname == '/about'}
-	<MetaTags title="About | Prabhu Kiran Konda" />
+	<MetaTags title="About | Prabhu Kiran Konda" description="About Me" />
 {/if}
