2f0d79981e62c5a5bbf12dcd289d9e0b4f55d242
Prabhu Kiran Konda <prabhukiran8790@pm.me>
2023-11-23 18:25:36 +0530

    changed icon rendering method in codeblocks in md:

--- src/lib/components/markdown/layout.svelte
+++ src/lib/components/markdown/layout.svelte
@@ -35,8 +35,8 @@
 <svelte:head>
 	<link
 		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.css"
-		integrity="sha384-zTROYFVGOfTw7JV7KUu8udsvW2fx4lWOsCEDqhBreBwlHI4ioVRtmIvEThzJHGET"
+		href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"
+		integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV"
 		crossorigin="anonymous"
 	/>
 </svelte:head>
