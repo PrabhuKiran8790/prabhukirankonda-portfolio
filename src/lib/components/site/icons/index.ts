4c739990d6141b590c9cb7e9da7ac7c908f7682a
XADE <56511165+imxade@users.noreply.github.com>
2024-03-04 19:38:05 +0000

    fixed vercel error; don't know what was causing it

--- src/lib/components/site/icons/index.ts
+++ src/lib/components/site/icons/index.ts
@@ -1,5 +1,6 @@
 export { default as Moon } from './moon.svelte';
 export { default as X } from './X.svelte';
+export { default as CodeBerg } from './codeberg.svelte';
 export { default as LinkedIn } from './linkedin.svelte';
 export { default as Svelte } from './svelte.svelte';
 export { default as Streamlit } from './streamlit.svelte';
@@ -7,5 +8,4 @@ export { default as MySQL } from './mysql.svelte';
 export { default as Tailwind } from './tailwind.svelte';
 export { default as Drizzle } from './drizzle.svelte';
 export { default as Planet } from './planet.svelte';
-export { default as CodeBerg } from './codeberg.svelte';
 export { default as SQL } from './sql.svelte';
