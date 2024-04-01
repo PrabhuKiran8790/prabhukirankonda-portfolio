389130bea942645f88a2e7d4ae33762434010b8f
XADE <56511165+imxade@users.noreply.github.com>
2024-04-01 11:43:14 +0000

    synced dependencies with upstream

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+// import adapter from '@sveltejs/adapter-vercel';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from '@huntabyte/mdsvex';
